807e26b1cf4962959ba2c3c1b28992d161844d3f
Hariom Yadav <hoc.yadav@gmail.com>
2020-11-06 09:08:15 +0530

    added ptr

--- goptr/goptr1.go
+++ goptr/goptr1.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	x:= 10
+	funcName(x)
+	fmt.Println(x)// print
+
+	funName2(&x)// pass pointer
+	fmt.Println(x)// print old value
+}
+
+func funName2(i *int) {
+	(*i) = (*i)*2 //dereference it and use it
+}
+
+func funcName(x int) {
+	x = x*2
 }
