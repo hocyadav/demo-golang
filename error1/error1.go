64c41d2b10d8f7a6f29d59b263263170ec0c6a6d
Hariom Yadav <hoc.yadav@gmail.com>
2020-11-06 09:07:32 +0530

    added error

--- error1/error1.go
+++ error1/error1.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"fmt"
+	"math"
+	"net/http"
+)
+
 func main() {
-	$END$
+	value, err := sqrt(9)
+	if err != nil {
+		fmt.Println("error")
+	}else {
+		fmt.Println(value)
+	}
+
+	value1, err := sqrt(-9)
+	if err != nil {
+		fmt.Println("error")
+	}else {
+		fmt.Println(value1)
+	}
+	contentType, err := contentTest("https://www.google.com")
+	if err != nil {
+		fmt.Println("error")
+	} else {
+		fmt.Println(contentType)
+	}
+}
+
+func sqrt(x float64) (float64, error) { //1. return 2 values , error object
+	if x < 0 {
+		return 0.0, fmt.Errorf("less than 0 (%f)", x)
+	}
+	return math.Sqrt(x), nil
+}
+
+func contentTest(url string)  (string, error){
+	response, err := http.Get(url)
+	if err != nil { // check if error
+		return "", err
+	}
+	// no error then close the body and get required body
+	defer response.Body.Close() // close the body
+
+	contentLength := response.Header.Get("Content-Type")
+	if contentLength == "" {
+		return "", fmt.Errorf("cant find content Type")
+	}else {
+		return contentLength, nil
+	}
 }
