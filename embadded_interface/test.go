fe28ecf5888f61140509f5d6232d6ac70d879030
Hariom Yadav <hoc.yadav@gmail.com>
2020-11-13 09:40:39 +0530

    added

--- embadded_interface/test.go
+++ embadded_interface/test.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-
+// create 2 interface , and one interface that contain these 2 interface
 type interface1 interface {
 	bangBang()
 }
@@ -13,6 +13,7 @@ type interface2 interface {
 type finalinterface interface {
 	interface1
 	interface2
+	//hiBye()
 }
 
 type Student struct {
@@ -37,10 +38,27 @@ func main() {
 		Age:     31,
 		Address: "bangalore",
 	}
+	// calling via object
 	student.bangBang()
 
 	student.hiBye()
 
+	var f finalinterface
+	f = &Student{
+		Name:    "chandan",
+		Age:     24,
+		Address: "bangalore",
+	}
+	//calling via interface
+	f.bangBang()
+	f.hiBye()
 
+	f2 := &Student{
+		Name:    "chandan",
+		Age:     24,
+		Address: "bangalore",
+	}
 
+	f2.bangBang()
+	f2.hiBye()
 }
