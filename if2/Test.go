012db760be7f648f8ce5a00e6fcf8a580a243c88
Hariom Yadav <hoc.yadav@gmail.com>
2020-11-13 09:41:37 +0530

    added

--- if2/Test.go
+++ if2/Test.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+
+	if err := myfun(); err != nil {// if block with multiple lines
+		fmt.Println(" error : ", err)
+	} else {
+		fmt.Println("no error..")
+	}
+}
+
+func myfun() error {
+	return fmt.Errorf("myfun error !!")
 }
