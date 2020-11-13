4bf219c2bb191fcf77b423728c51e7d711dc8ba7
Hariom Yadav <hoc.yadav@gmail.com>
2020-11-13 09:40:27 +0530

    added

--- fun/Test.go
+++ fun/Test.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
+type Order struct {
+	Val1 int
+	Val2 string
+}
+// syntax : receiver + fun name with input argument [optional]+ return type [optional]
+func (o *Order) fun1() int {//always pass address
+	fmt.Println(o.Val1)
+	return o.Val1 + 10
+}
+
 func main() {
-	$END$
+	order := Order{
+		Val1: 123,
+		Val2: "hariom",
+	}
+	x := order.fun1()//class with method fun1
+	fmt.Println(x)
 }
