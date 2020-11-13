11c1a4d85b79646944254f9856457c486afe40af
Hariom Yadav <hoc.yadav@gmail.com>
2020-11-13 09:40:48 +0530

    added

--- file/Test.go
+++ file/Test.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	//"github.com/pelletier/go-toml" //working
+	//toml "github.com/pelletier/go-toml" //working
+	toml1 "github.com/pelletier/go-toml"
+	"log"Sq
+	"os"
+)
+
+type Config struct {
+	Login struct {
+		User     string
+		Password string
+	}
+}
+
 func main() {
-	$END$
+	//open file
+
+	file, err := os.Open("/Users/hariomyadav/go/src/awesomeProject1/file/file1.toml")
+	if err != nil {
+		log.Fatalf("error occur when opening file - %s", err)
+	}
+	defer file.Close()
+	//close file using defer
+
+	// create a new config obj
+	configObj := &Config{}
+
+	decoder := toml1.NewDecoder(file)// create a decoder obj using file , and using decode obj decode our class obj i.e. COnfig
+
+	if err := decoder.Decode(configObj); err != nil {
+		log.Fatalf("error .. %s", err)
+	}
+	fmt.Printf("config %+v \n", configObj)
 }
