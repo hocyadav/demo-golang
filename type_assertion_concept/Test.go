4579d8e04dd6a42e0bc99b51f8f5ddc1eb9e02b9
Hariom Yadav <hoc.yadav@gmail.com>
2020-11-13 09:41:44 +0530

    added

--- type_assertion_concept/Test.go
+++ type_assertion_concept/Test.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+// https://www.geeksforgeeks.org/interfaces-in-golang/
 func main() {
-	$END$
+	var a interface{} = "hariom"
+	myfun(a)
+}
+
+func myfun(a interface{}) {
+	extractedValue := a.(string)
+	fmt.Println(extractedValue)
 }
